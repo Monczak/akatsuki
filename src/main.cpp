cd592cbf39b466d69da3ec5d2abb9d43b2578aa5
Monczak <szymon.jedrzejczak.sj@gmail.com>
2025-02-20 01:33:08 +0100

    Add clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -7,39 +7,38 @@ SDL_Window* window;
 SDL_Renderer* renderer;
 bool running = true;
 
-namespace akatsuki {
-
-void main_loop() 
+void main_loop()
 {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
-        if (event.type == SDL_QUIT) running = false;
+        if (event.type == SDL_QUIT)
+            running = false;
     }
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 
-    #ifdef __EMSCRIPTEN__
-    if (!running) emscripten_cancel_main_loop();
-    #endif
+#ifdef __EMSCRIPTEN__
+    if (!running)
+        emscripten_cancel_main_loop();
+#endif
 }
 
-int main() 
+int main()
 {
     SDL_Init(SDL_INIT_VIDEO);
     window = SDL_CreateWindow("akatsuki", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    #ifdef __EMSCRIPTEN__
+#ifdef __EMSCRIPTEN__
     emscripten_set_main_loop(main_loop, 0, 1);
-    #else
-    while (running) main_loop();
-    #endif
+#else
+    while (running)
+        main_loop();
+#endif
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
-}
-
 }
